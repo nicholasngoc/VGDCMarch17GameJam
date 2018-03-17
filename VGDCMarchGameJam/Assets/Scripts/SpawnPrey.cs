73cdac782fed59a7e0df5a575c2a4495664e0074
Nicholas Nguyen <nicholas.ngoc.nguyen@gmail.com>
2018-03-17 12:59:31 -0700

    Spawner

---
+++ VGDCMarchGameJam/Assets/Scripts/SpawnPrey.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPrey : MonoBehaviour {
+
+    public int spawnCount;
+    public int numPrey;
+    public GameObject[] prey;
+    public GameObject environment;
+    public float respawnRate;
+
+    float currentTime;
+
+    private void Awake()
+    {
+        currentTime = 0f;
+        numPrey = 0;
+    }
+
+    void Update () {
+        
+        if(currentTime > respawnRate && numPrey < spawnCount)
+        {
+            spawnPrey();
+            numPrey++;
+            currentTime = 0f;
+        }
+        else
+        {
+            currentTime += Time.deltaTime;
+        }
+	}
+
+    void spawnPrey()
+    {
+        Vector3 ranPosition = new Vector3(Random.Range(environment.transform.position.x - environment.transform.lossyScale.x / 2, environment.transform.position.x + environment.transform.lossyScale.x / 2),
+            Random.Range(environment.transform.position.y - environment.transform.lossyScale.y / 2, environment.transform.position.y + environment.transform.lossyScale.y / 2),
+            Random.Range(environment.transform.position.z - environment.transform.lossyScale.z / 2, environment.transform.position.z + environment.transform.lossyScale.z / 2));
+        //Debug.Log(ranPosition);
+
+        Quaternion ranRotation = Random.rotation;
+
+        int ranPrey = Random.Range(0, prey.Length - 1);
+
+        Instantiate(prey[ranPrey], ranPosition, ranRotation);
+    }
+}
