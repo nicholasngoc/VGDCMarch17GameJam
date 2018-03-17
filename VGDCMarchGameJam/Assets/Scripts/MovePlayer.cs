03408069d38a15a5b6b54ac628d587397b986cd8
Nicholas Nguyen <nicholas.ngoc.nguyen@gmail.com>
2018-03-17 11:42:31 -0700

    Added movement and light stuff

--- VGDCMarchGameJam/Assets/Scripts/MovePlayer.cs
+++ VGDCMarchGameJam/Assets/Scripts/MovePlayer.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float rotateSpeed;
+    public float movementSpeed;
+
+    Rigidbody rb;
+    float horizontal;
+    float vertical;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+		transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, -Input.GetAxis("Mouse X")) * Time.deltaTime * rotateSpeed);
+        //transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey("w"))
+        {
+            rb.velocity = transform.up * (vertical * movementSpeed);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = transform.right * (horizontal * movementSpeed);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = transform.right * (horizontal * movementSpeed);
+        }
+    }
 }
