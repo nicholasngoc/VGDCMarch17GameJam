80e5b8e818e2e76184b03ccbdcddab6cae61a122
cwripley1 <37487895+cwripley1@users.noreply.github.com>
2018-03-17 16:49:53 -0700

    added worms_eaten_display

---
+++ VGDCMarchGameJam/Assets/Scripts/Worms_Eaten_Display.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Worms_Eaten_Display : MonoBehaviour {
+
+    public Text myText2;
+    public Global global;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myText2.text = "worms eaten: " + global.numEaten;
+	}
+}
